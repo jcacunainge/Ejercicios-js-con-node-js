60d757ea85b04f4392a8f2cf502ec482d34f2cad
Jhan Carlos Acuña Ordoñez <jcacunainge@gmail.com>
2022-10-17 15:42:25 -0500

    Actualizar ejercicios

---
+++ manejoDOM/scripts.js
@@ -0,0 +1,18 @@
+
+//Uso de getElementById , obtener elementos html por medio de id
+
+let cabecero = document.getElementById('cabecero').innerHTML;
+console.log('Valor cabecero:'+cabecero);
+
+let parrafo = document.getElementById('parrafo');
+console.log('Valor cabecero:' +parrafo.innerHTML);
+
+//Realizar cambio al documento median js
+//parrafo.innerHTML = 'Nuevo Valor del Parrafo';
+//cabecero.innerHTML = 'Nuevo cabecero';
+
+
+//Uso de 
+
+
+

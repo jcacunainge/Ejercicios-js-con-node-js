89f686c5bf6a95fc664eaa0401ffa686725aa165
Jhan Carlos Acuña Ordoñez <jcacunainge@gmail.com>
2022-10-22 15:23:30 -0500

    Manejo de DOM FORMULARIO DE TAREAS

---
+++ manejoDOM/script.js
@@ -0,0 +1,26 @@
+const btn = document.querySelector('[data-form-btn]');
+
+//Arrow functions o funciones anonimas
+const createTask = (evento) => {
+    evento.preventDefault() //Borrar lo que agreguemos en el input 
+    const input = document.querySelector('[data-form-input]');
+    const value = input.value;
+    const list = document.querySelector('[data-list]');
+    const task = document.createElement('li');
+    task.classList.add("card")
+    input.value = "";
+    const content = `
+    <div>
+        <i class="far fa-check-square icon"></i>
+        <span class="task">${value}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    task.innerHTML=content;
+    list.appendChild(task);
+    console.log(content);   
+}
+
+console.log(btn);
+btn.addEventListener("click", createTask);
+;
+

83e12656ad420745ee9af12fe2d6d14691019dad
Jhan Carlos Acuña Ordoñez <jcacunainge@gmail.com>
2022-10-29 20:15:34 -0500

    Terminar Proyecto Manejo DOM

--- manejoDOM/script.js
+++ manejoDOM/script.js
@@ -1,32 +1,29 @@
-const btn = document.querySelector('[data-form-btn]');
 
+import deleteIcon from "./componets/deleteIcon.js";
+import checkComplete from "./componets/checkComplete.js";
+
+const btn = document.querySelector('[data-form-btn]');
 //Arrow functions o funciones anonimas
+
 const createTask = (evento) => {
     evento.preventDefault() //Borrar lo que agreguemos en el input 
     const input = document.querySelector('[data-form-input]');
     const value = input.value;
     const list = document.querySelector('[data-list]');
-    const task = document.createElement('li');
-    task.classList.add("card")
-    input.value = "";
-    const content = `
-    <div>
-        ${checkComplete()}
-        <span class="task">${value}</span>
-    </div>
-    <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    task.innerHTML=content;
-    list.appendChild(task);
-    console.log(content);   
-}
-console.log(btn);
-btn.addEventListener("click", createTask);
+    const task = document.createElement('li'); //se crea el elemento li, que contiene la tarea e iconos
+    task.classList.add("card"); //Se le agrega la clase a el elemnto li
+    input.value = ""; 
+    const taskContent = document.createElement("div"); //Se crea el contenedor div
 
-const checkComplete = () => {
-    const i = document.createElement("i");
-    i.classList.add("far");
-    i.classList.add("fa-check-square");
-    i.classList.add("icon");
-    return i;
+    const titleTask = document.createElement('span');
+    titleTask.classList.add("task");
+    titleTask.innerText = value; // Lo que se coloca en el input
+    taskContent.appendChild(checkComplete()); // se creo anteriormente la funcion y se coloca dentro del div, con la funcion indicada al inicio
+    taskContent.appendChild(titleTask);
+
+    task.appendChild(taskContent);
+    list.appendChild(task);
+    task.appendChild(deleteIcon());
 };
 
+btn.addEventListener("click", createTask); //crea la tarea al darle cl

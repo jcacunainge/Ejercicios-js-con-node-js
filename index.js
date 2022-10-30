6f73f8da76d264f47689602433e7361836596912
Jhan Carlos Acuña Ordoñez <jcacunainge@gmail.com>
2022-10-29 21:18:05 -0500

    inicio programación orientada a objetos

---
+++ index.js
@@ -0,0 +1,20 @@
+class Cliente {
+    nombreCliente;
+    dniCliente;
+    rutCliente;
+}
+
+class CuentaCorriente{
+    numeroCuentaCliente;
+    saldoCuentaCliente;
+}
+
+const cliente1 = new Cliente;
+const cuenta1 = new CuentaCorriente;
+
+cliente1.nombreCliente= "Jhan";
+cliente1.dniCliente= 33333;
+cliente1.rutCliente= 55555;
+
+cuenta1.numeroCuentaCliente= 1212;
+cuenta1.saldoCuentaCliente= 1000;

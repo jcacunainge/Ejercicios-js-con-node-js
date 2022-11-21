5b4184da05256a0d252ea3b4a4dbd25c9cd64b00
Jhan Carlos Acuña Ordoñez <102251412+jcacunainge@users.noreply.github.com>
2022-11-21 15:27:35 -0600

    Actualizacion

--- carritoCompras/app.js
+++ carritoCompras/app.js
@@ -2,18 +2,20 @@ const items = document.getElementById("items");
 const templateCard = document.getElementById("template-card").content;
 const fragment = document.createDocumentFragment();
 
+let carrito = {};
+
 document.addEventListener("DOMContentLoaded", ()=> {
     fetchData();
 });
 
 items.addEventListener("click", e =>{
-    addCarrito(e)
+    addCarrito(e);
 })
 
 const fetchData = async () => {
     try {
         const res = await fetch('api.json')
-        const data = await res.json()
+        const data = await res.json();
         //console.log(data)
         pintarCards(data)
     } catch (error){
@@ -24,19 +26,36 @@ const fetchData = async () => {
 const pintarCards = data =>{
     //Recorremos el array data, de nuestro json, para separarlos y leerlo uno por uno, con sus respectivos elementos... precios, img, titulo.
     data.forEach(producto => {
-        templateCard.querySelector('img').setAttribute("src", producto.thumbnailUrl)
-        templateCard.querySelector('h5').textContent = producto.title
-        templateCard.querySelector('p').textContent = producto.precio
-        templateCard.querySelector('.btn-dark').dataset.id = producto.id
 
-
+        templateCard.querySelector('img').setAttribute("src", producto.thumbnailUrl);
+        templateCard.querySelector('h5').textContent = producto.title;
+        templateCard.querySelector('p').textContent = producto.precio;
+        templateCard.querySelector('.btn-dark').dataset.id = producto.id;
         const clone = templateCard.cloneNode(true)
         fragment.appendChild(clone)
     });
+
     items.appendChild(fragment)
 } 
 
 
 const addCarrito = e =>{
+    //console.log(e.target)
+    //console.log(e.target.classList.contains("btn-dark"))
+    if(e.target.classList.contains("btn-dark")){
+        
+        //console.log(e.target.parentElement) compramos lo que selecionamos
+        setCarrito(e.target.parentElement)
+    }
+    e.stopPropagation()
+}
 
+const setCarrito = objeto =>{
+    const producto = {
+        id: objeto.querySelector(".btn-dark").dataset.id,
+        title: objeto.querySelector("h5").textContent,
+        precio: objeto.querySelector("p").textContent,
+        cantidad: 1    
+    }
+    
 }
